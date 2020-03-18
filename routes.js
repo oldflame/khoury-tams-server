9f18d6d3e3c201706043b859ce3e63ff2f7fdeab
Dhaval Dedhia <dhavl32@gmail.com>
2020-03-18 01:01:53 -0400

    Added more details for course API

--- routes.js
+++ routes.js
@@ -3,4 +3,5 @@ var course = require("./services/course");
 exports = module.exports = app => {
   app.get("/courses/:stream", course.getCoursesForStream);
   app.get("/courses", course.getAllCourses);
+  app.get("/courses/details/:crn",course.getMoreCourseDetails);
 };
--- services/course.js
+++ services/course.js
@@ -13,7 +13,7 @@ var course = {
   getCoursesForStream: (req, res) => {
     const streamValue = req.params.stream;
     console.log("Fetching courses for: ", streamValue);
-    req.app.db.models.Course.find({ stream: streamValue}, (err, courses) => {
+    req.app.db.models.Course.find({ stream: streamValue }, (err, courses) => {
       if (err) {
         console.log("Error", err);
         return res.json([]);
@@ -21,6 +21,19 @@ var course = {
       console.log("Courses", courses);
       return res.status(200).json(courses);
     }).select("_id Course CRN Title Instructors stream");
+  },
+
+  getMoreCourseDetails: (req, res) => {
+    const crn = req.params.crn;
+    console.log("Details for: ", crn);
+    req.app.db.models.Course.find({CRN: crn}, (err,courses)=> {
+        if(err){
+            console.log("Error",err);
+            return res.json([]);
+        }
+        console.log("Course Details",courses);
+        return res.status(200).json(courses);
+    })
   }
 };
 
