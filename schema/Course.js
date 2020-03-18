040ccfa90196d038a6ece97cf5bdcdbddccfea58
Dhaval Dedhia <dhavl32@gmail.com>
2020-03-18 00:06:54 -0400

    Changed DB

--- schema/Course.js
+++ schema/Course.js
@@ -4,7 +4,9 @@ exports = module.exports = function(app, mongoose) {
   var devSchema = new mongoose.Schema({
     _id: { type: mongoose.Schema.Types.ObjectId, unique: true },
     title: String,
+    CRN: String,
     code: String,
+    Instructors: String,
     stream: String
     // added_by : {type: mongoose.Schema.Types.ObjectId, ref: 'Admin'}
   });
